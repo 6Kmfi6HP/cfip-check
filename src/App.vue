39f74b7669f8b3f15c9e4db98ac2a208bb7a8fd4
3Kmfi6HP <58669916+3Kmfi6HP@users.noreply.github.com>
2023-08-21 10:26:07 +0000

    ok

--- src/App.vue
+++ src/App.vue
@@ -14,22 +14,23 @@
                     <div class="field">
                         <label class="label">server</label>
                         <div class="control">
-                            <input class="input" type="text" placeholder="Enter server domain or ip" v-model="servDomain">
+                            <input class="input" type="text" placeholder="Enter server domain or ip"
+                                v-model="servDomain">
                         </div>
                     </div>
 
-<div class="field">
-                    <label class="label">tls</label>
-                    <div class="control">
-                        <label>
-                            <input type="radio" name="tls" value="true" v-model="tls" checked/> 
-                            true
-                   
-                            <input type="radio" name="tls" value="false" v-model="tls" />
-                            false
-                        </label>
+                    <div class="field">
+                        <label class="label">tls</label>
+                        <div class="control">
+                            <label>
+                                <input type="radio" name="tls" value="true" v-model="tls" checked />
+                                true
+
+                                <input type="radio" name="tls" value="false" v-model="tls" />
+                                false
+                            </label>
+                        </div>
                     </div>
-                </div>
 
                     <div class="field">
                         <label class="label">host</label>
@@ -55,69 +56,91 @@
 
     </main>
 </template>
-  
+
 <script>
-export default {
-    data() {
-        return {
-            servDomain: 'cdn.xn--b6gac.eu.org:443',
-            host: 'speed.cloudflare.com',
-            result: '',
-            tls: true
-        }
-    },
-
-    methods: {
-        onSubmit() {
-          let parts = this.servDomain.split(':')
-
-          let ip = parts[0]
-          let port = parts[1] || 443
-
-          console.log('IP:', ip)  
-          console.log('Port:', port)
-          // ?ip=1.1.1.1&port=443&tls=true&host=cloudflare.com
-            const url = '/api?ip='+ ip +'&host=' + this.host + '&port=' + port +'&tls='+ this.tls
-            // get site url 
-            const siteUrl = window.location.origin;
-
-            fetch(url, {
-                method: "GET"
-            })
-                .then((response) => {
-                    response.text().then((data) => {
-                        this.result = data;
-                        // this.subscribe_url = siteUrl + url + '&format=base64';
-                        console.log("data" + data + url)
-                    });
-                })
-                .catch((err) => {
-                    console.error(err);
-                    this.result = err;
-                });
+    export default {
+        data() {
+            return {
+                servDomain: 'cdn.xn--b6gac.eu.org:443',
+                host: 'speed.cloudflare.com',
+                result: '',
+                tls: true
+            }
         },
+        mounted() {
+            const params = new URLSearchParams(window.location.search)
+            if (params.has('ip')) {
+                this.ip = params.get('ip')
+            }
 
+            if (params.has('host')) {
+                this.host = params.get('host')
+            }
+
+            if (params.has('port')) {
+                this.port = params.get('port')
+            }
+
+            if (params.has('tls')) {
+                this.tls = params.get('tls')
+            }
+            if (this.ip && this.host && this.port && this.tls) {
+                // 执行提交逻辑
+                this.onSubmit()
+            }
+        },
+        methods: {
+            onSubmit() {
+                let parts = this.servDomain.split(':')
+
+                let ip = parts[0]
+                let port = parts[1] || 443
+
+                console.log('IP:', ip)
+                console.log('Port:', port)
+                // ?ip=1.1.1.1&port=443&tls=true&host=cloudflare.com
+                const url = '/api?ip=' + ip + '&host=' + this.host + '&port=' + port + '&tls=' + this.tls
+                const surl = '/?ip=' + ip + '&host=' + this.host + '&port=' + port + '&tls=' + this.tls
+                // get site url 
+                const siteUrl = window.location.origin;
+
+                fetch(url, {
+                    method: "GET"
+                })
+                    .then((response) => {
+                        response.text().then((data) => {
+                            this.result = '\n\n' + data;
+                            this.subscribe_url = siteUrl + surl;
+                            console.log("data" + data + url)
+                        });
+                    })
+                    .catch((err) => {
+                        console.error(err);
+                        this.result = err;
+                    });
+            },
+
+        }
     }
-}
 
 </script>
-    
+
 <style>
-.margin-bottom {
-    margin-bottom: 15px;
-}
+    .margin-bottom {
+        margin-bottom: 15px;
+    }
 
-.fade-enter,
-.fade-leave-active {
-    opacity: 0;
-}
+    .fade-enter,
+    .fade-leave-active {
+        opacity: 0;
+    }
 
-.fade-enter-active,
-.fade-leave-active {
-    transition: opacity 0.5s;
-}
+    .fade-enter-active,
+    .fade-leave-active {
+        transition: opacity 0.5s;
+    }
 
-pre {
-    white-space: pre-wrap;
-}
+    pre {
+        white-space: pre-wrap;
+    }
 </style>
