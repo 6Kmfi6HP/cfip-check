dc7d1943193955c247401aebd921c195aff7e0e7
3Kmfi6HP <58669916+3Kmfi6HP@users.noreply.github.com>
2023-12-18 02:33:50 +0700

    feat(App): update server input label and add form submission from mounted hook
    Adjusted the label of the "server" input field to "IP" for clarity. Also added logic in the mounted hook to handle form submission when all required parameters (IP, host, port, and tls) are present in the URL. This ensures that the form is automatically submitted upon loading the page with the necessary parameters.
    
    Note: The provided diff does not include any changes related to the template or style elements.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
- <header class="navbar">
+    <header class="navbar">
 
-  <h1 class="navbar-item">
-    <a href="https://github.com/3Kmfi6HP">3Kmfi6HP</a> 
-  </h1>
+        <h1 class="navbar-item">
+            <a href="https://github.com/3Kmfi6HP">3Kmfi6HP</a>
+        </h1>
 
-</header>
+    </header>
 
     <main class="section">
         <h1 class="title">Deta.app cf ip remote check</h1>
@@ -13,13 +13,12 @@
         <div class="columns">
 
             <div class="column">
-                <form @submit.prevent="onSubmit">
+                <form @submit.prevent="onSubmit" ref="form">
 
                     <div class="field">
-                        <label class="label">server</label>
+                        <label class="label">IP</label>
                         <div class="control">
-                            <input class="input" type="text" placeholder="Enter server domain or ip"
-                                v-model="servDomain">
+                            <input class="input" type="text" placeholder="Enter server domain or ip" v-model="servDomain">
                         </div>
                     </div>
 
@@ -39,8 +38,7 @@
                     <div class="field">
                         <label class="label">host</label>
                         <div class="control">
-                            <input class="input" type="text" placeholder="Enter host must same sni in cf"
-                                v-model="host">
+                            <input class="input" type="text" placeholder="Enter host must same sni in cf" v-model="host">
                         </div>
                     </div>
 
@@ -62,89 +60,91 @@
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                servDomain: 'cdn.xn--b6gac.eu.org:443',
-                host: 'speed.cloudflare.com',
-                result: '',
-                tls: true
-            }
-        },
-        mounted() {
-            const params = new URLSearchParams(window.location.search)
-            if (params.has('ip')) {
-                this.ip = params.get('ip')
-            }
-
-            if (params.has('host')) {
-                this.host = params.get('host')
-            }
-
-            if (params.has('port')) {
-                this.port = params.get('port')
-            }
-
-            if (params.has('tls')) {
-                this.tls = params.get('tls')
-            }
-            if (this.ip && this.host && this.port && this.tls) {
-                // 执行提交逻辑
-                this.onSubmit()
-            }
-        },
-        methods: {
-            onSubmit() {
-                let parts = this.servDomain.split(':')
-
-                let ip = parts[0]
-                let port = parts[1] || 443
-
-                console.log('IP:', ip)
-                console.log('Port:', port)
-                // ?ip=1.1.1.1&port=443&tls=true&host=cloudflare.com
-                const url = '/api?ip=' + ip + '&host=' + this.host + '&port=' + port + '&tls=' + this.tls
-                const surl = '/?ip=' + ip + '&host=' + this.host + '&port=' + port + '&tls=' + this.tls
-                // get site url 
-                const siteUrl = window.location.origin;
-
-                fetch(url, {
-                    method: "GET"
-                })
-                    .then((response) => {
-                        response.text().then((data) => {
-                            this.result = '\n\n' + data;
-                            this.subscribe_url = siteUrl + surl;
-                            console.log("data" + data + url)
-                        });
-                    })
-                    .catch((err) => {
-                        console.error(err);
-                        this.result = err;
-                    });
-            },
-
+export default {
+    data() {
+        return {
+            servDomain: '',
+            host: 'speed.cloudflare.com',
+            result: '',
+            tls: true
         }
+    },
+    mounted() {
+        const params = new URLSearchParams(window.location.search)
+        if (params.has('ip')) {
+            this.ip = params.get('ip')
+        }
+
+        if (params.has('host')) {
+            this.host = params.get('host')
+        }
+
+        if (params.has('port')) {
+            const port = params.get('port')
+            this.servDomain = params.get('ip') + ':' + port
+        }
+
+        if (params.has('tls')) {
+            this.tls = params.get('tls')
+        }
+        if (this.ip && this.host && this.port && this.tls) {
+            // 执行提交逻辑
+            this.onSubmit()
+            this.$refs.form.submit()
+        }
+    },
+    methods: {
+        onSubmit() {
+            let parts = this.servDomain.split(':')
+
+            let ip = parts[0]
+            let port = parts[1] || 443
+
+            console.log('IP:', ip)
+            console.log('Port:', port)
+            // ?ip=1.1.1.1&port=443&tls=true&host=cloudflare.com
+            const url = '/api?ip=' + ip + '&host=' + this.host + '&port=' + port + '&tls=' + this.tls
+            const surl = '/?ip=' + ip + '&host=' + this.host + '&port=' + port + '&tls=' + this.tls
+            // get site url 
+            const siteUrl = window.location.origin;
+
+            fetch(url, {
+                method: "GET"
+            })
+                .then((response) => {
+                    response.text().then((data) => {
+                        this.result = '\n\n' + data;
+                        this.subscribe_url = siteUrl + surl;
+                        console.log("data" + data + url)
+                    });
+                })
+                .catch((err) => {
+                    console.error(err);
+                    this.result = err;
+                });
+        },
+
     }
+}
 
 </script>
 
 <style>
-    .margin-bottom {
-        margin-bottom: 15px;
-    }
+.margin-bottom {
+    margin-bottom: 15px;
+}
 
-    .fade-enter,
-    .fade-leave-active {
-        opacity: 0;
-    }
+.fade-enter,
+.fade-leave-active {
+    opacity: 0;
+}
 
-    .fade-enter-active,
-    .fade-leave-active {
-        transition: opacity 0.5s;
-    }
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.5s;
+}
 
-    pre {
-        white-space: pre-wrap;
-    }
+pre {
+    white-space: pre-wrap;
+}
 </style>
